c5dc817a88d14aa2885e730fedd148cd37f45c45
Matthias <vidalmat06@gmail.com>
2021-01-25 16:51:30 +0100

    construction du site

---
+++ index.php
@@ -0,0 +1,26 @@
+<?php
+
+
+
+
+
+
+
+
+
+?>
+
+
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="templates/style.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Agence</title>
+</head>
+<body>
+    
+</body>
+</html>

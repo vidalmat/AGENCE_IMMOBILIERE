aa414fb1519a7d331ba5c5fab9ccf9f484e45e99
Matthias <vidalmat06@gmail.com>
2021-03-24 19:43:48 +0100

    ajouts des modifs du menu burger en responsive

--- templates/espace_agent.php
+++ templates/espace_agent.php
@@ -1,25 +1,15 @@
 <?php
 
-
-
 var_dump($_POST);
-
-
-
 
 ?>
 
 
 <header>
-
     <div class="conteneur1">
-
         <div class="logo_header">
-
             <img src="images/logo.png" alt="logo">
-
         </div>
-
         <div class="nav_user">
             <nav>
                 <ul>
@@ -27,71 +17,77 @@ var_dump($_POST);
                     <li class="mon_compte"><a href="index.php?route=showform">Mon compte</a></li>
                 </ul>
             </nav>
-            
         </div>
-    
     </div>
-
-    <div class="menu_header">
+    <div class="row head_small"> <!-- création du bouton du menu de déroulement -->
+        <div class="hamburger_form col-2">
+            <a href="#menu">
+                <span></span> <!--les span servent pour créer des traits dans le menu -->
+                <span></span>
+                <span></span>
+            </a>
+        </div>
+    </div>
+    <div id="menu" class="menu_header">
         <nav>
             <ul>
-                <li><a href="#">Acheter</a></li>
-                <li><a href="#">Louer</a></li>
-                <li><a href="#">Vendre</a></li>
-                <li><a href="#">Estimer</a></li>
-                <li><a href="#">Faire gérer</a></li>
-                <li><a href="#">Syndic</a></li>
-                <li><a href="#">Immobilier pro</a></li>
-                <li class="agence_rouge"><a href="#">Nos agences</a></li>
+                <li><a href="#">&times;</a></li><!-- création de la croix pour la fermeture du menu -->
+                <li><a href="#">Acheter</a><span></span></li>
+                <li><a href="#">Louer</a><span></span></li>
+                <li><a href="#">Vendre</a><span></span></li>
+                <li><a href="#">Estimer</a><span></span></li>
+                <li><a href="#">Faire gérer</a><span></span></li>
+                <li><a href="#">Syndic</a><span></span></li>
+                <li><a href="#">Immobilier pro</a><span></span></li>
+                <li class="agence_vert"><a href="#">Nos agences</a><span></span></li>
             </ul>
         </nav>
     </div>
-
-
 </header>
-
-<p class="fildar">abri-nature-immobilier / <a href="index.php?route=showhome">accueil /</a><span class="span"> mon espace agent</span></p>
-
-
-<form action="index.php?route=insert_bien" method="POST" enctype="multipart/form-data">
-
-    <select name="categorie" id="">
-        <option value="1">Vente</option>
-        <option value="2">Location</option>
-    </select>
-    <input type="text" placeholder="Nom" name="nom" id="nom">
-    <input type="text" placeholder="Adresse" name="adresse" id="adresse">
-    <input type="text" placeholder="Prix" name="prix" id="prix">
-    <input type="file" name="image">
-    <input type="submit" value="Ajouter un bien" id="ajout_bien">
-
-</form>
+<section>
+    <p class="fildar">abri-nature-immobilier / <a href="index.php?route=showhome">accueil /</a><span class="span"> mon espace agent</span></p>
 
 
+    <form action="index.php?route=insert_bien" method="POST" enctype="multipart/form-data">
 
-<!--Affichage des biens-->
+        <select name="categorie" id="">
+            <option value="1">Vente</option>
+            <option value="2">Location</option>
+        </select>
+        <input type="text" placeholder="Nom" name="nom" id="nom">
+        <input type="text" placeholder="Adresse" name="adresse" id="adresse">
+        <input type="text" placeholder="Prix" name="prix" id="prix">
+        <input type="file" name="image">
+        <input type="submit" value="Ajouter un bien" id="ajout_bien">
 
-<?php
-$biens = $toTemplate["biens"];
-?>
+    </form>
 
-<table>
-    <tr>
-        <th>Image</th>
-        <th>Nom</th>
-        <th>Adresse</th>
-        <th>Prix</th>
-        <th>Action</th>
-    </tr>
 
-    <?php foreach($biens as $bien): ?>
+
+    <!--Affichage des biens-->
+
+    <?php
+    $biens = $toTemplate["biens"];
+    ?>
+
+    <table>
         <tr>
-        <td><img width="120" src="upload/<?= $bien["image"] ?>" alt="image"></td>
-        <td><?= $bien["nom"] ?></td>
-        <td><?= $bien["adresse"] ?></td>
-        <td><?= $bien["prix"] ?></td>
-        <td>Modifier | <a href="index.php?route=sup_bien&id_bien=<?= $bien["id_bien"] ?>"> Supprimer</a></td>
+            <th>Image</th>
+            <th>Nom</th>
+            <th>Adresse</th>
+            <th>Prix</th>
+            <th>Action</th>
         </tr>
 
-    <?php endforeach ?>
-</table>
+        <?php foreach($biens as $bien): ?>
+            <tr>
+            <td><img width="120" src="upload/<?= $bien["image"] ?>" alt="image"></td>
+            <td><?= $bien["nom"] ?></td>
+            <td><?= $bien["adresse"] ?></td>
+            <td><?= $bien["prix"] ?></td>
+            <td>Modifier | <a href="index.php?route=sup_bien&id_bien=<?= $bien["id_bien"] ?>"> Supprimer</a></td>
+            </tr>
+
+        <?php endforeach ?>
+    </table>
+</section>
--- templates/espace_membre.php
+++ templates/espace_membre.php
@@ -12,15 +12,10 @@
 
 
 <header>
-
     <div class="conteneur1">
-
         <div class="logo_header">
-
             <img src="images/logo.png" alt="logo">
-
         </div>
-
         <div class="nav_user">
             <nav>
                 <ul>
@@ -28,27 +23,32 @@
                     <li class="mon_compte"><a href="index.php?route=showform">Mon compte</a></li>
                 </ul>
             </nav>
-            
         </div>
-    
     </div>
-
-    <div class="menu_header">
+    <div class="row head_small"> <!-- création du bouton du menu de déroulement -->
+        <div class="hamburger_form col-2">
+            <a href="#menu">
+                <span></span> <!--les span servent pour créer des traits dans le menu -->
+                <span></span>
+                <span></span>
+            </a>
+        </div>
+    </div>
+    <div id="menu" class="menu_header">
         <nav>
             <ul>
-                <li><a href="#">Acheter</a></li>
-                <li><a href="#">Louer</a></li>
-                <li><a href="#">Vendre</a></li>
-                <li><a href="#">Estimer</a></li>
-                <li><a href="#">Faire gérer</a></li>
-                <li><a href="#">Syndic</a></li>
-                <li><a href="#">Immobilier pro</a></li>
-                <li class="agence_rouge"><a href="#">Nos agences</a></li>
+                <li><a href="#">&times;</a></li><!-- création de la croix pour la fermeture du menu -->
+                <li><a href="#">Acheter</a><span></span></li>
+                <li><a href="#">Louer</a><span></span></li>
+                <li><a href="#">Vendre</a><span></span></li>
+                <li><a href="#">Estimer</a><span></span></li>
+                <li><a href="#">Faire gérer</a><span></span></li>
+                <li><a href="#">Syndic</a><span></span></li>
+                <li><a href="#">Immobilier pro</a><span></span></li>
+                <li class="agence_vert"><a href="#">Nos agences</a><span></span></li>
             </ul>
         </nav>
     </div>
-
-
 </header>
 
 <p class="fildar">abri-nature-immobilier / <a href="index.php?route=showhome">accueil /</a><span class="span"> mon espace</span></p>
